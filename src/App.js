3b294132e534278ead8fe7d308fdffa285353fec
IrinaKotlyarova <i.kotlyarova@dunice.net>
2019-11-12 10:06:40 +0300

    gitignore

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  array = [
+    {
+      id: Math.random(),
+      status: false,
+      text: 'text'
+    },
+    {
+      id: Math.random(),
+      status: true,
+      text: 'text'
+    },
+    {
+      id: Math.random(),
+      status: false,
+      text: 'text'
+    },
+    {
+      id: Math.random(),
+      status: false,
+      text: 'text'
+    }];
 
+  arrayToJSX() {
+    const arrForShow = this.array.map(item => {
+      return <li id={item.id}>
+        <input type="checkbox" className="checkbox" />
+          <span id="toDo">{item.text}</span>
+          <input type="button" id="X" className="button-x" value="X" />
+      </li>
+    });
+
+    return(arrForShow);
+  }
+
+  render() {
+    return (
+        <div>{this.arrayToJSX()}</div>
+    )
+  }
+}
 export default App;
